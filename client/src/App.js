483413476adaba70df25c001699b46e29f259a10
Amy Gardiner <amymgardiner@gmail.com>
2022-10-04 18:02:10 -0500

    added error page aka no match page

--- client/src/App.js
+++ client/src/App.js
@@ -38,6 +38,8 @@ function App() {
               <Route path="/signup" element={<Signup />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/thought" element={<SingleThought />} />
+
+              <Route path="*" element={<NoMatch />} />
             </Routes>
           </div>
           <Footer />
