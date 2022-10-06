368490811a3525037c4ce827138c4e2a1ff9d85d
Amy Gardiner <amymgardiner@gmail.com>
2022-10-06 12:42:01 -0500

    added logged out function for the header so the nav bar changes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+  // remove the token from localStorage
+  // then refresh the application by taking the user back to the homepage
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  // user has to log in to gain access to user-based features in the nav bar
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
@@ -10,8 +19,19 @@ const Header = () => {
         </Link>
 
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
